7119204f7e389fe23de5088867a9ee97649c10fa
Taha969 <taha1428mm@gmail.com>
2025-04-04 19:36:16 +0200

    Password test.py

---
+++ Password_test.py
@@ -0,0 +1,14 @@
+import re
+
+def is_strong_password(pwd):
+    pattern = '^[a-z]+[A-Z]+[!@#$^%]+[0-9]+$'
+    return bool(re.search(pattern, pwd))
+
+while True:
+    password = input("Enter your password: ")
+
+    if is_strong_password(password):
+        print("Strong password")
+        break
+    else:
+        print("\n Password is weak, enter another one ")
